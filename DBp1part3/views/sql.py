069c1e7059774d8739e5d429a4636f8d8ee90bf2
MarionetteSHF <hanfu.shi2019@gmail.com>
2022-03-19 17:50:08 -0400

    connectToDB

--- DBp1part3/views/sql.py
+++ DBp1part3/views/sql.py
@@ -1,11 +1,26 @@
-
 import psycopg2
-conn = psycopg2.connect(database='proj1part2',user='hs3239',password='7505',host="127.0.0.1", port="5000")
+host_name = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
+user_name = "hs3239"
+password = "7505"
+
+
+engine = psycopg2.connect(
+    database="proj1part2",
+    user=user_name,
+    password=password,
+    host=host_name,
+    port='5432'
+)
+# import psycopg2
+# conn = psycopg2.connect(database='proj1part2',user='hs3239',password='7505',host="127.0.0.1", port="5000")
+cur = engine.cursor()
+cur.execute("SELECT * FROM Users")
+
 # cur = conn.cursor()
 # cur.execute("SELECT * FROM table1 LIMIT 10")
-# rows = cur.fetchall()
-# print(rows)
+rows = cur.fetchall()
+print(rows)
 # conn.commit()
 # cur.close()
 # conn.close()
-print("Opened database successfully")
+# print("Opened database successfully")
