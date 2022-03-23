a7cd5d5f18351632d0b98926938593ef5d2d9faa
Huili Zheng <hz2710@cumc.columbia.edu>
2022-03-23 17:49:17 -0400

    create py

--- DBp1part3/views/post.py
+++ DBp1part3/views/post.py
@@ -1,30 +1,43 @@
 from flask import Blueprint,flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
-from .login import auth
+from .test2 import auth
+
 from DBp1part3 import sql
 bp = Blueprint('post', __name__)
 
 @bp.route('/create', methods=('GET', 'POST'))
-@login_required
+@auth
 def create():
     if request.method == 'POST':
+        posted_at = request.form['posted_at']
         title = request.form['title']
-        body = request.form['body']
+        price = request.form['price']
+        number = request.form['number']
+        category = request.form['category']
+        wantorsell = request.form['wantorsekk']
+        description = request.form['description']
         error = None
 
         if not title:
             error = 'Title is required.'
-
+        if not price:
+            error = 'Price is required.'
+        if not wantorsell:
+            error = 'Want it or need it?'
+        if not number:
+            error = 'Number is required.'
+        if not category:
+            error = 'Category is required.'
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO Items_Posted (posted_at, user_id, title, description, price, category, number, neededItem)'
+                'VALUES (%D, %d, %s, %s, %f, %s, %d, %s)',
+                (posted_at, Now(), session['user_id'], title, description, price, category, number, neededItem)
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('index'))
 
-    return render_template('blog/create.html')
+    return render_template('create.html')
